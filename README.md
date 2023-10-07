51f7b71553e7bd48f6082ec1428cd7929abb4bd9
Lucas Pereira <lucaspereira0497@gmail.com>
2023-10-07 15:50:25 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vote classifier 
+
+This project aims to be an webapplication built with streamlit in order to make predictions on who you would vote for based on socio-geographics features. 
+
+This work is based on data archives and collection made by Cagé & Piketty for the book : [une histoire du conflit politique](https://www.unehistoireduconflitpolitique.fr/)
+
+## How to use it ?
+
+Work in progress .. App not ready
--- voteclassification/app/app.py
+++ voteclassification/app/app.py
@@ -6,8 +6,6 @@ st.set_page_config(
     page_icon=":envelope:",
 )
 
-st.sidebar.success("Pages")
-
 st.title("Devine pour qui tu vas voter ! :envelope:")
 
 st.markdown(
@@ -22,5 +20,27 @@ Toutes les données utilisées leur appartiennent et sont l'issu d'un travail de
 ## Que fait cette application ?
 
 Le but est de développer un petit modèle de machine learning afin de prédire en fonction de paramètres socio-géographiques, le vote d'un citoyen français ! :fr:
+
             """
 )
+
+st.markdown(
+    """"
+            Remplis ces critères afin de faire une prédiction ! 
+            """
+)
+
+
+with st.form("my_form"):
+    st.write("")
+    slider_val = st.slider("", help="")
+    checkbox_val = st.checkbox("", help="")
+
+    submitted = st.form_submit_button("Envoyez !")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.markdown("---")
+
+st.markdown("Code source [github](https://github.com/lucarammel/voteclassifier)")
+st.markdown("Auteur : [Lucas PEREIRA](https://github.com/lucarammel)")
